3f12680fee1aec6c075ba3368bdcbbb6ddc87cab
JamesOmare <jamesomare922@gmail.com>
2023-09-26 08:12:22 +0300

    added additional info to About page

--- components/About.jsx
+++ components/About.jsx
@@ -15,7 +15,8 @@ const About = () => {
           <p className='py-2 text-gray-600'>
             As a full stack developer with expertise in both frontend and backend technologies, 
             I bring a valuable skill set to any development project. My proficiency in frontend 
-            technologies such as JavaScript, React, and Next.js allows me to create engaging and 
+            technologies such as HTML, CSS and JavaScript alongside frameworks such as React and Next.js
+            allows me to create engaging and 
             responsive user interfaces, while my knowledge of backend technologies such as Python 
             and Java enables me to design and implement scalable, efficient, and secure server-side 
             solutions. My ability to seamlessly integrate these technologies allows me to build complex
@@ -24,12 +25,15 @@ const About = () => {
             within budget.
           </p>
           <p className='py-2 text-gray-600'>
-          As a recent graduate in Information Technology, I bring a fresh perspective and up-to-date knowledge to my role as a full stack developer.
-          With my academic training and hands-on experience, I am well-equipped to take on the challenges of the modern web development landscape. 
-          I have a keen eye for detail and a passion for creating high-quality, user-focused solutions.
-          I am also a strong collaborator and can work effectively as part of a team, communicating complex technical concepts to both technical 
-          and non-technical stakeholders. My diverse skill set and dedication to my craft make me eager to contribute my talents to the development
-           of innovative and impactful software solutions.
+          As a professional with three years of experience and a degree in Information Technology,
+          I bring a seasoned perspective and current knowledge to my role as a full stack developer.
+          Through my academic foundation and hands-on experience, I've honed my skills and am well-prepared
+          to tackle the challenges presented by today's dynamic software development landscape.
+          I have a meticulous attention to detail and a deep passion for crafting top-notch, user-centric solutions.
+          Furthermore, my adeptness as a collaborator allows me to function effectively within a team, effectively
+          conveying intricate technical concepts to both technical and non-technical stakeholders. My multifaceted
+          skill set and unwavering commitment to my field make me enthusiastic about contributing my expertise to 
+          the creation of innovative and impactful software solutions.
           
           </p>
           <Link href='/#projects'>
